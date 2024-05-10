3c9a9db658e17351b4c0fd67b68a0c1eeb441a76
rachel4w2 <rachel4w2@naver.com>
2024-05-10 10:25:27 +0900

    msw 초기 세팅

---
+++ src/main.tsx
@@ -0,0 +1,24 @@
+import React from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App";
+
+async function enableMocking() {
+  if (process.env.NODE_ENV !== "development") {
+    return;
+  }
+
+  const { worker } = await import("./mocks/browser");
+
+  return worker.start({
+    onUnhandledRequest: "bypass",
+  });
+}
+
+enableMocking().then(() => {
+  const root = createRoot(document.getElementById("root") as HTMLElement);
+  root.render(
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  );
+});
